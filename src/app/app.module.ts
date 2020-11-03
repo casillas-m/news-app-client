6eb3e00fde99f2844e0dcc13312a00a3f3fe6de3
casillas-m <si715612@iteso.mx>
2020-11-02 21:04:52 -0600

    app pages schema changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewsComponent } from './pages/news/news.component';
 import { MenuComponent } from './menu/menu.component';
+import { HeadlinesComponent } from './pages/headlines/headlines.component';
+import { Page404Component } from './pages/page404/page404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewsComponent,
-    MenuComponent
+    MenuComponent,
+    HeadlinesComponent,
+    Page404Component
   ],
   imports: [
     BrowserModule,
