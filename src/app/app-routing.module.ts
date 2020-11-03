975c5bad241b959c2a50ba7c3604edd1203b67c3
casillas-m <si715612@iteso.mx>
2020-11-02 21:03:40 -0600

    change angular router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeadlinesComponent } from "./pages/headlines/headlines.component"
+import { NewsComponent } from "./pages/news/news.component"
+import { Page404Component } from "./pages/page404/page404.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"news", pathMatch:"full"},
+  {path:"headlines",component:HeadlinesComponent},
+  {path:"news",component:NewsComponent},
+  {path:"**",component:Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
